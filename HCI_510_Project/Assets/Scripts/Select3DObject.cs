91a7e4ed5d34756b08e7e22c28a3e53a6fe94f6c
Anjali <agali1@iastate.edu>
2024-04-17 16:55:52 -0500

    Made gameobjects that face you

--- HCI_510_Project/Assets/Scripts/Select3DObject.cs
+++ HCI_510_Project/Assets/Scripts/Select3DObject.cs
@@ -4,18 +4,45 @@ using UnityEngine;
 
 public class Select3DObject : MonoBehaviour
 {
-    private Renderer renderer;
+    public GameObject canvasObject;
+
+    // private Transform transform;
+    private Camera mainCamera;
+    private Renderer _renderer;
+    private Color prevColor;
+    private bool click = true;
     // Start is called before the first frame update
     void Start()
     {
-        renderer = GetComponent<Renderer>();
+        _renderer = GetComponent<Renderer>();
     }
 
-    // Update is called once per frame
+
+    // OnMouseDown is called once per frame
     void OnMouseDown()
     {
         Debug.Log("Click!");
-        renderer.material.color =
-            renderer.material.color == Color.red ? Color.blue : Color.red;
+        _renderer.material.color =
+            _renderer.material.color == Color.blue ? Color.yellow : Color.blue;
+        if (click)
+        {
+            InstantiateNameTag();
+        }
+        else
+        {
+            DestroyNameTag();
+        }
+        click = !click;
+    }
+
+    void InstantiateNameTag()
+    {
+        GameObject objToInstantiate = Instantiate(canvasObject);
+        objToInstantiate.transform.SetPositionAndRotation(new Vector3(_renderer.transform.position.x, _renderer.transform.position.y + 0.5f, _renderer.transform.position.z), new Quaternion(0,0,0,0));
+    }
+    void DestroyNameTag()
+    {
+        GameObject objToDestroy = GameObject.FindGameObjectWithTag("tag");
+        Destroy(objToDestroy);
     }
 }
